3639d73a13eed7071a975142fd7c30f2ac9a8118
Paul D. Duncan <chesiredarling@gmail.com>
2020-01-30 21:45:52 -0800

    Dont forget to update user in postman!

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
@@ -12,7 +15,6 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
-
 
 const PORT = process.env.PORT || 5000;
 
