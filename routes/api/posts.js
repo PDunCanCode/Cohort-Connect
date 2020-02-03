f5a45bfbb3bc78fc138e30045ffc9db43899c523
Paul D. Duncan <chesiredarling@gmail.com>
2020-02-02 21:26:16 -0800

    posts.js going up

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
 
-// @route GET api/post
-// @desc test route
-//@access Public
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-router.get('/', (req, res) => res.send('post Route'));
+// @route POST api/post
+// @desc Create a post
+//@access Private
+
+router.get(
+  '/',
+  [
+    auth,
+    [
+      check('text')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+      }
+
+      try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+        
+        const post = await newPost.save();
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.messge);
+        res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
