105c94e77633c9e1dfe494d724328e9ffeb46fa3
Paul D. Duncan <chesiredarling@gmail.com>
2020-02-12 15:17:47 -0800

    Education build out and imported into Profile.js

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import ProfileTop from './ProfileTop';
 
 import { getProfileById } from '../../actions/profile';
@@ -45,6 +47,21 @@ const Profile = ({
                 </Fragment>
               ) : (
                 <h4>No experience credentials</h4>
+              )}
+            </div>
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
               )}
             </div>
           </div>
